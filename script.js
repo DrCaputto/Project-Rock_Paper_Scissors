7d368630ccb25088784ce981c4e5aaa209f10b90
DrCaputto <branko.92.pop@gmail.com>
2022-01-14 22:15:41 +0100

    added the Play again? button, added live score with messages of the outcome, started work on the overlay with a button that will pop up when one player reaches 5 points. It's still a work in progress. After which the final visual work will begin

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const bod = document.getElementById('bod');
 const butDiv = document.getElementById('butDiv');
 const butRok = document.getElementById('rock');
 const butPap = document.getElementById('paper');
 const butScs = document.getElementById('scissors');
+const divScor = document.getElementById('scores');
+const divOps = document.getElementById('oponents');
+const divPlSc = document.getElementById('player_score');
+const divPcSc = document.getElementById('computer_score');
+const divOut = document.getElementById('outcome');
+const btnRest = document.getElementById('play_again');
+const popLog = document.getElementById('popup_log');
+// const overlay = document.getElementById('overlay');
+const gameOver = document.getElementById('game_over_popup');
 
 butRok.addEventListener('click', playRound);
 butPap.addEventListener('click', playRound);
 butScs.addEventListener('click', playRound);
+// overlay.addEventListener('click', EndEndGame);
 
 let scrPl=0;
 let scrPc=0;
-
-let pointPl = 0;
-let pointPc = 0;
 
 function computerPlay (){
     const arr=['rock', 'paper', 'scissors'];
@@ -27,50 +33,55 @@ function computerPlay (){
 function playRound(event) {
     let computerSelection = computerPlay();
     let playerSelection = event.target.getAttribute('potez');
+    let log ='';
     
     if (playerSelection === computerSelection) {
-        alert ("A match was struck... yet no blood was spilt...");
+        log = "A match was struck... yet no winner has emerged...";
     } else if (
         (playerSelection === 'paper' && computerSelection === 'rock') || 
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')
         ) {
-            pointPl = 1;
-            scrPl += pointPl;
+            scrPl++;
+            log = "You won this round!"
     } else if (
         (playerSelection === 'paper' && computerSelection === 'scissors') ||
         (playerSelection === 'rock' && computerSelection === 'paper') ||
         (playerSelection === 'scissors' && computerSelection === 'rock')
         ){
-            pointPc = 1;
-            scrPc += pointPc;
+            scrPc++;
+            log = "You lost this round!"
         }
-
+        
+    divPlSc.textContent = "You won: " + scrPl + " rounds!" ;
+    divPcSc.textContent = "PC has won: " + scrPc + " rounds!";
+    divOut.textContent = log;
+    
+    if (scrPl==5 && scrPc < 5) {
+        divOut.textContent = log + " Now, with the total of 5 victories, you have bested your PC and became the superior being!";
+        resetGame ();
+    } else if (scrPl < 5 && scrPc == 5 ) {
+        divOut.textContent = log + " You fought well but were no match for the procesing power of your PC!";
+        resetGame ();
+    } 
+    
     console.log(scrPl)
     console.log(scrPc)
-    return scrPl && scrPc;
 }
 
-const rezDiv = document.createElement('div');   
-const parResTit = document.createElement('p');
+function resetGame () {
+    btnRest.addEventListener('click', () => {
+        document. location. reload();
+    })
+}
 
-const text = document.createTextNode("Rezultat: " + scrPl + " - " + scrPc);
-parResTit.appendChild(text);
-rezDiv.appendChild(parResTit);
-
-
-bod.appendChild(rezDiv);
-
-
-
-// Funkcija ispod je namenjena za loop igre 5 puta; privremeno je gasim jer cu da pokusam da napravim igru tako da se radi koliko god puta korisnik klikce
-
-// function game () {
-//     for (let i = 1; i<=5; i++) {
-//         playRound();
-//         // computerPlay ();
-//         // let computerSelection = computerPlay();
-
-//     }
+// function StartEndGame () {
+//     gameOver.classList.add('active');
+//     overlay.classList.add('active');
+//     resetGame ();
 // }
-// game ();
+
+// function EndEndGame () {
+//     gameOver.classList.remove('active');
+//     overlay.classList.remove('active');
+// }
